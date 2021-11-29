54b4375febdc7ea09d671662fbc151d09db820c9
i0Exception <andrewomagnuson@gmail.com>
2021-11-29 11:53:24 -0600

    Converted to gradle fully. Changed mute to use discords new timeout feature. Updated JDA to 5.0.0

--- README.md
+++
@@ -1,1 +0,0 @@
-# DiscordBot_v4
--- bukkit/src/main/java/com/i0dev/discordbot/bukkit/BukkitMain.java
+++ bukkit/src/main/java/com/i0dev/discordbot/bukkit/BukkitMain.java
@@ -4,15 +4,10 @@ import com.i0dev.discordbot.Heart;
 import com.i0dev.discordbot.object.StartupTag;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
-import oshi.SystemInfo;
-import oshi.software.os.OSProcess;
 
-import java.lang.management.ManagementFactory;
-import java.text.NumberFormat;
 import java.util.Arrays;
 
 public class BukkitMain extends JavaPlugin {
-
     Heart heart;
 
     @Override
@@ -21,10 +16,6 @@ public class BukkitMain extends JavaPlugin {
                 StartupTag.BUKKIT,
                 StartupTag.PLUGIN
         ), Bukkit.getLogger(), this);
-        Bukkit.getScheduler().runTaskTimerAsynchronously(this, () -> {
-            OSProcess process = new SystemInfo().getOperatingSystem().getProcess(Integer.parseInt(ManagementFactory.getRuntimeMXBean().getName().split("@")[0]));
-            heart.logDebug("Used Memory: " + "" + NumberFormat.getIntegerInstance().format(process.getResidentSetSize() / 1024 / 1024) + " MB" + "");
-        }, 20 * 5, 20 * 15);
     }
 
     @Override
