78be29f57d5dcb41620d0bd9a5b0fcb827e5d035
i0dev <AndrewOMagnuson@gmail.com>
2022-03-21 17:35:41 -0500

    Added /ip command

---
+++ common/src/main/java/com/i0dev/discordbot/command/general/CmdIP.java
@@ -0,0 +1,54 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) i0dev
+ * Copyright (c) contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package com.i0dev.discordbot.command.general;
+
+import com.i0dev.discordbot.Heart;
+import com.i0dev.discordbot.object.abs.CommandEventData;
+import com.i0dev.discordbot.object.abs.DiscordCommand;
+import com.i0dev.discordbot.object.builder.EmbedMaker;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+public class CmdIP extends DiscordCommand {
+
+    public CmdIP(Heart heart) {
+        super(heart);
+    }
+
+    @Override
+    protected void setupCommand() {
+        setCommand("ip");
+        setDescription("Gets the server IP.");
+    }
+
+    @Override
+    public void execute(SlashCommandInteractionEvent e, CommandEventData data) {
+        data.replySuccess(heart.cnf().getCmdIpContent());
+    }
+
+}
